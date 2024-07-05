e940a74ca9085d40b8a7a68d2a59c0e6cbbef262
kiannaquines <kjgnaquines@gmail.com>
2024-07-05 17:27:14 +0800

    add more CRUD functionality

--- core/app/app_views/app_views.py
+++ core/app/app_views/app_views.py
@@ -1,34 +1,58 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from app.models import *
+from app.forms import *
+from django.http import HttpResponseRedirect
+from django.contrib import messages
 
+# DONE
 @login_required(login_url="/auth/login/")
 def supplier_index(request):
     suppliers = SupplierModel.objects.all()
+    supplier_form = SupplierForm()
 
     context = {
         'suppliers':suppliers,
+        'supplier_form': supplier_form,
     }
 
+    if request.method == "POST":
+        supplier_form = SupplierForm(request.POST)
+        if supplier_form.is_valid():
+            supplier_form.save()
+            messages.success(request,"You have added new supplier, thank you!",extra_tags="add_success")
+            return HttpResponseRedirect("/suppliers/")        
+        
     return render(request,"supplier.html",context)
 
 @login_required(login_url="/auth/login/")
 def users_index(request):
     users = CustomUser.objects.all()
-
     context = {
         'users': users
     }
 
+    if request.method == "POST":
+        pass
+
     return render(request,"users.html",context)
 
-
+# DONE
 @login_required(login_url="/auth/login/")
 def category_index(request):
     categories = CategoryModel.objects.all()
+    category_form = CategoryForm()
     context = {
         'categories':categories,
+        'category_form': category_form,
     }
+
+    if request.method == "POST":
+        category_form = CategoryForm(request.POST)
+        if category_form.is_valid():
+            category_form.save()
+            messages.success(request,"You have added new category, thank you!",extra_tags="add_success")
+            return HttpResponseRedirect("/category/")
 
     return render(request,"category.html",context)
 
@@ -64,10 +88,22 @@ def products_index(request):
 @login_required(login_url="/auth/login/")
 def warehouseproducts_index(request):
     warehouseproducts = WarehouseProductModel.objects.all()
+    warehouseproducts_form = WarehouseProductForm()
 
     context = {
         'warehouseproducts':warehouseproducts,
+        'warehouseproducts_form': warehouseproducts_form,
     }
+
+    if request.method == "POST":
+        warehouseproducts_form = WarehouseProductForm(request.POST,request.FILES)
+        if warehouseproducts_form.is_valid():
+            warehouseproducts_form.save()
+            messages.success(request,"You have added new warehouse product, thank you!",extra_tags="add_success")
+            return HttpResponseRedirect("/warehouseproducts/")
+        else:
+            messages.error(request,"Something went wrong, please try again.",extra_tags="some_error")
+            return HttpResponseRedirect("/warehouseproducts/")
 
     return render(request,"warehouseproducts.html",context)
 
--- core/app/forms.py
+++ core/app/forms.py
@@ -4,10 +4,52 @@ from app.models import *
 
 class SupplierForm(ModelForm):
     def __init__(self, *args,**kwargs):
-        super(SupplierForm,self).init(*args,**kwargs)
-        self.fields['supplier_companyname'].widget.attrs.update({'class':'form-control'})
+        super(SupplierForm,self).__init__(*args,**kwargs)
+        self.fields['supplier_companyname'].widget.attrs.update({'class':'form-control','spellcheck':'false','placeholder':'Company Name'})
         self.fields['supplier_companyname'].label = "Company Name"
+        self.fields['supplier_address'].widget.attrs.update({'class':'form-control','spellcheck':'false','placeholder':'Address','rows':'2'})
+        self.fields['supplier_address'].label = "Address"
+        self.fields['supplier_mobilenumber'].widget.attrs.update({'class':'form-control','spellcheck':'false','placeholder':'Mobile No.'})
+        self.fields['supplier_mobilenumber'].label = "Mobile Number"
 
     class Meta:
         model = SupplierModel
         fields = ['supplier_companyname','supplier_address','supplier_mobilenumber']
+
+
+class CategoryForm(ModelForm):
+    def __init__(self,*args,**kwargs):
+        super(CategoryForm,self).__init__(*args,**kwargs)
+        self.fields['category_name'].widget.attrs.update({'class':'form-control','spellcheck':'false','placeholder':'Category Name'})
+
+    class Meta:
+        model = CategoryModel
+        fields = ['category_name']
+
+
+class WarehouseProductForm(ModelForm):
+    def __init__(self,*args,**kwargs):
+        super(WarehouseProductForm,self).__init__(*args,**kwargs)
+        
+        self.fields['warehouse_product_name'].widget.attrs.update({'class':'form-control','spellcheck':'false','placeholder':'Warehouse Product Name'})
+        self.fields['warehouse_product_name'].label = "Warehouse Product Name"
+
+        self.fields['warehouse_product_stock'].widget.attrs.update({'class':'form-control','spellcheck':'false','placeholder':'Stock'})
+        self.fields['warehouse_product_stock'].label = "Stock"
+
+        self.fields['warehouse_product_description'].widget.attrs.update({'class':'form-control','spellcheck':'false','placeholder':'Description','rows':'2'})
+        self.fields['warehouse_product_description'].label = "Description"
+
+        self.fields['warehouse_product_category'].widget.attrs.update({'class':'form-control','spellcheck':'false'})
+        self.fields['warehouse_product_category'].label = "Category"
+
+
+        self.fields['warehouse_product_supplier'].widget.attrs.update({'class':'form-control','spellcheck':'false'})
+        self.fields['warehouse_product_supplier'].label = "Supplier"
+
+        self.fields['warehouse_product_picture'].widget.attrs.update({'class':'form-control','spellcheck':'false'})
+        self.fields['warehouse_product_picture'].label = "Product Image"
+
+    class Meta:
+        model = WarehouseProductModel
+        fields = ['warehouse_product_name','warehouse_product_stock','warehouse_product_description','warehouse_product_category','warehouse_product_picture','warehouse_product_supplier',]
