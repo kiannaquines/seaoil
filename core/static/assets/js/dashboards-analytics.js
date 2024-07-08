1df22635cef3597fa8c05321b9654881033eda08
kiannaquines <kjgnaquines@gmail.com>
2024-07-08 21:52:20 +0800

    add more improvement to the system

--- core/static/assets/js/dashboards-analytics.js
+++ core/static/assets/js/dashboards-analytics.js
@@ -8,34 +8,51 @@
   axisColor = config.colors.axisColor;
   borderColor = config.colors.borderColor;
 
+  // Total Revenue Chart
   const totalRevenueChartElement = document.querySelector('#totalRevenueChart');
-
   fetch("fetchTotalSalesMonthly/")
   .then(response => response.json())
   .then(jsonData => {
+
     const years = Object.keys(jsonData);
     const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
     const series = years.map(year => ({
-      name: year,
+      name: 'Total Revenue '+ year,
       data: months.map(month => {
         const entry = jsonData[year].find(item => item.month === month);
         return entry ? entry.total : 0;
       })
     }));
 
-    const options = {
+    var options = {
       series: series,
       chart: {
         height: 350,
-          type: 'area',
-          fontFamily: 'Public Sans, Arial, sans-serif',
+        type: 'area',
+        fontFamily: 'Public Sans, Arial, sans-serif',
       },
       dataLabels: {
         enabled: false
       },
       stroke: {
         curve: 'smooth'
+      },
+      fill: {
+        type: 'gradient',
+        gradient: {
+          shadeIntensity: 1,
+          opacityFrom: 0.7,
+          opacityTo: 0.9,
+          stops: [0, 100]
+        }
+      },
+      tooltip: {
+        shared: true,
+        intersect: false,
+        onDatasetHover: {
+          highlightDataSeries: false,
+        }
       },
       colors: [config.colors.warning,config.colors.primary],
       xaxis: {
@@ -55,12 +72,17 @@
           }
         }
       },
-    };
+      grid:{
+        show: true,
+        strokeDashArray: 5,
+      }
+    };  
 
     var totalRevenueChart = new ApexCharts(totalRevenueChartElement, options);
     totalRevenueChart.render();
   }).catch(error => console.error('Error fetching data:', error));
 
+  // Product Monthly In
   const productInChartElement = document.querySelector('#productMonthlyIn');
   fetch('fetchTotalProductIn/')
     .then(response => response.json())
@@ -84,6 +106,22 @@
         stroke: {
           curve: 'smooth'
         },
+        fill: {
+          type: 'gradient',
+          gradient: {
+            shadeIntensity: 1,
+            opacityFrom: 0.7,
+            opacityTo: 0.9,
+            stops: [0, 100]
+          }
+        },
+        tooltip: {
+          shared: true,
+          intersect: false,
+          onDatasetHover: {
+            highlightDataSeries: false,
+          }
+        },
         colors: [config.colors.primary],
         xaxis: {
           categories: categories,
@@ -102,8 +140,12 @@
             }
           }
         },
+        grid:{
+          show: true,
+          strokeDashArray: 5,
+        }
       };  
-      const productInChart = new ApexCharts(productInChartElement, options);
+      var productInChart = new ApexCharts(productInChartElement, options);
       productInChart.render();
     }).catch(error => console.error('Error fetching data:', error));
 })();
