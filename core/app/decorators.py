182390b6162be387d18912ad80409d3ba83e62e2
kiannaquines <kjgnaquines@gmail.com>
2024-07-11 00:04:09 +0800

    add decorators for accessability on the system

--- core/app/decorators.py
+++ core/app/decorators.py
@@ -8,3 +8,34 @@ def check_already_loggedin(view_func):
             return view_func(request,*args,**kwargs)
     
     return _wrapped_view
+
+
+def check_if_can_edit(view_func):
+    def _wrapped_view(request,*args,**kwargs):
+        user = request.user
+        if user.is_authenticated and user.is_staff:
+            return view_func(request,*args,**kwargs)
+        else:
+            return redirect('/')
+    
+    return _wrapped_view
+
+def check_if_can_delete(view_func):
+    def _wrapped_view(request,*args,**kwargs):
+        user = request.user
+        if user.is_authenticated and user.is_staff:
+            return view_func(request,*args,**kwargs)
+        else:
+            return redirect('/')
+    
+    return _wrapped_view
+
+def check_if_can_view(view_func):
+    def _wrapped_view(request,*args,**kwargs):
+        user = request.user
+        if user.is_authenticated and user.is_staff:
+            return view_func(request,*args,**kwargs)
+        else:
+            return redirect('/')
+    
+    return _wrapped_view
